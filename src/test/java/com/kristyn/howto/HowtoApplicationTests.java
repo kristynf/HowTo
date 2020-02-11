e8a3e6ed68e35f916aa33be6476f849d581f4f8b
kristynf <thetruthandsoul@gmail.com>
2020-02-11 15:20:57 -0500

    initial commit

---
+++ src/test/java/com/kristyn/howto/HowtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.howto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
